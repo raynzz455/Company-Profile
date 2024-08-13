930d3562467d1e06387f01a990436d79896a6970
raynaldi <raynaldi.qwerty@gmail.com>
2024-08-13 12:13:21 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
 <script>
-import "../app.css";
-import Home from '../lib/components/home/home.svelte';
-import Cons from '../lib/components/content/content.svelte';
-import Porto from '../lib/components/portofolio/porto.svelte';
-import Footer from '../lib/components/footer/footer.svelte';
-
-</script>
-<body> 
- <style>
+    import "../app.css";
+    import Home from '../lib/components/home/home.svelte';
+    import Cons from '../lib/components/content/content.svelte';
+    import Porto from '../lib/components/portofolio/portofolio.svelte';
+    import Footer from '../lib/components/footer/footer.svelte';
     
- </style>
-    <Home />
-    <Cons />
-    <Porto />
-    <Footer />
-
-</body>
-
+    </script>
+    <body> 
+     <style>
+        
+     </style>
+    <div class="w-full overflow-hidden">
+        <Home />
+        <Cons />
+        <Porto />
+        <Footer />
+    </div>
+    
+    </body>
+    
+    
