3d0c9906874d45f423bfc519803ad7cdc4bb058c
erza <satriaseptian724@gmail.com>
2024-08-21 18:59:21 +0700

    terserah

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,489 +1,155 @@
-  <script>
+<script>
   import "../../../app.css";
-    import terserah from "$lib/images/logo-icon.png";
-    import { onMount } from 'svelte';
-    import Splide from '@splidejs/splide';
-    import '@splidejs/splide/dist/css/splide.min.css';
-    
-    let splide;
+  import terserah from "$lib/images/logo-icon.png";
+  import { onMount } from 'svelte';
+  import Splide from '@splidejs/splide';
+  import '@splidejs/splide/dist/css/splide.min.css';
 
-    onMount(() => {
-      splide = new Splide('.splide', {
-        type       : 'loop',
-        perPage    : 1,
-        perMove    : 1,
-        autoplay   : true,
-        pagination : true,
-        arrows     : false,
-        interval   : 2500, // Set interval to 2 seconds
-      }).mount();
-    });
-  </script>
+  let splide;
 
+  onMount(() => {
+    splide = new Splide('.splide', {
+      type       : 'loop',
+      perPage    : 1,
+      perMove    : 1,
+      autoplay   : true,
+      pagination : true,
+      arrows     : false,
+      interval   : 2500, // Set interval to 2 seconds
+    }).mount();
+  });
+</script>
 
-  <style>
-    .splide {
-    width: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 0 20px;
-    position: relative;
-  }
-
-  .splide__slide {
-    display: flex;
-    justify-content: center; /* Center horizontally */
-    align-items: center;     /* Center vertically */
-    padding: 20px;
-    margin: 0 15px;
-    position: relative;
-    /* Apply transformation to shift slide content */
-    transform: translateX(-15px); /* Adjust this value to shift left if necessary */
-  }
-
-  .splide__pagination {
-    display: flex;
-    justify-content: center;
-    position: absolute;
-    bottom: -30px;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 10;
-    margin-bottom: 0;
-  }
-
-  .testimonial-container {
-    width: 100%;
-    text-align: center;
-    margin: 0 auto;
-    padding: 20px;
-    position: relative;
-    margin-bottom: 80px;
-  }
-
-  .testimonial-heading {
-    font-size: 37px;
-    font-weight: bold;
-    margin-bottom: 20px;
-    color: #333;
-  }
-
-  .testimonial-card {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #f28928;
-    border-radius: 15px;
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
-    padding: 30px;
-    text-align: center;
-    max-width: 500px;
-    width: 100%;
-    margin: 0 auto;
-    border: 1px solid #ddd;
-    color: #fff;
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-    /* Adjust this value to shift left */
-    transform: translateX(-15px); 
-  }
-
-  .arrow-bottom {
-    position: absolute;
-    width: 20px;
-    height: 20px; 
-    display: block;
-    background: #f28928;
-    transform: rotate(45deg);
-    top: 100%;
-    left: 50%;
-    margin-left: -10px;
-    margin-top: -10px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-
-  .testimonial-card:hover {
-    transform: translateY(-15px) translateX(-15px); /* Apply consistent transform on hover */
-    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.15);
-  }
-
-  .testimonial-content {
-    font-size: 18px;
-    color: #fff;
-    margin-bottom: 20px;
-  }
-
-  .testimonial-author {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .author-info {
-    text-align: left;
-  }
-
-  .author-name {
-    font-weight: bold;
-    margin: 0;
-    color: #fff;
-    font-size: 18px;
-  }
-
-  .author-role {
-    margin: 0;
-    color: #ddd;
-    font-size: 14px;
-  }
-
-  @media (max-width: 768px) {
-    .splide__slide {
-      margin: 0 5px;
-      /* Adjust translateX if needed */
-      transform: translateX(-5px);
-    }
-  }
-
-
-  /* CSS yang ada */
-.whatsapp-button {
-  display: flex;
-  align-items: center;
-  background-color: #25D366;
-  color: white;
-  padding: 0.5rem 1rem; /* Kecilkan padding untuk tombol utama */
-  border-radius: 50px;
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
-  transition: background-color 0.3s;
-  z-index: 1000;
-  border: 2px solid #25D366;
-}
-
-/* Posisi tombol WhatsApp pertama */
-.whatsapp-button.first-button {
-  bottom: 60px; /* Atur jarak dari bagian bawah */
-}
-
-/* Hover effect */
-.whatsapp-button:hover {
-  background-color: #128C7E;
-  border-color: #128C7E;
-}
-
-.whatsapp-button img {
-  width: 35px;
-  height: 35px;
-  margin-right: 1px;
-}
-
-    .kenapa-container {
-      text-align: center;
-      padding: 30px 20px;
-      background-color: #f9f9f9;
-      color: #333;
-      padding-top: 30px;
-    }
-
-    .kenapa-heading {
-      font-size: 2rem;
-      color: #000;
-      margin-bottom: 10px;
-    }
-
-    .text {
-      font-size: 1rem;
-      line-height: 1.6;
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 0;
-      text-align: center;
-    }
-
-    .text p {
-      margin: -5px;
-    }
-
-    @media (max-width: 768px) {
-      
-
-      .text {
-        font-size: 0.875rem;
-      }
-    }
-
-    @media (max-width: 768px) {
-    .whatsapp-button.second-button {
-      margin-bottom: 60px; /* Sesuaikan dengan ukuran layar lebih kecil */
-    }
-  }
-
-  .whatsapp-button.second-button {
-    display: flex;
-    align-items: center;
-    background-color: #25D366;
-    color: white;
-    padding: 15px 15px;
-    border-radius: 50px;
-    position: relative;
-    box-shadow: 0 4px 8px rgba(0,0,0,0.2);
-    transition: background-color 0.3s;
-    width: 250px;
-    margin: 100px auto 0px;
-    text-align: center; /* This centers the text inside */
-  }
-
-    .whatsapp-button.second-button:hover {
-      background-color: #128C7E;
-      border-color: #128C7E;
-    }
-
-    .whatsapp-button.second-button img {
-    width: 35px;
-    height: 35px;
-    margin-right: 10px;
-  }
-
-  .whatsapp-text {
-    font-size: 16px;
-    font-weight: bold;
-    text-align: center;
-    flex: 1; /* Ensures the text takes the remaining space */
-  }
-
-  .whyus-container{
-    margin-bottom: 20px;
-    color: #333;
-    padding: 40px;
-    background-color: #f28928;
-  }
-
-  .whyus-content{
-    font-size: 60px;
-    text-align: center;
-    text-shadow: #000;
-    text-decoration: double ;
-  }
-
-  .Atas{
-    text-align: center;
-    font-size: 17px;
-  }
-
-  .Teks1{
-  margin-top: 40px;
-  font-size: 20px;
-  }
-
-  .paragrap{
-    margin-left: 63vh;
-  }
-
-
-    .footer {
-      background-color: #333;
-      color: #fff;
-      padding: 40px 20px;
-      text-align: center;
-      height: 10rem;
-      position: relative;
-      overflow: hidden;
-      margin-top: 60px;
-    }
-
-    .footer-logo {
-      margin-bottom: -30px;
-    }
-
-    .footer-logo img {
-      max-width: 120px;
-    }
-
-    .footer-links a {
-      color: #f28928;
-      text-decoration: none;
-      font-size: 16px;
-      transition: color 0.3s;
-      margin-top: 20px;
-    }
-
-    .footer-links a:hover {
-      color: #ff8c00;
-    }
-
-    .footer-social {
-  margin-top: 0px;
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-  padding: 0 20px;
-}
-
-.footer-social a {
-  color: #fff;
-  font-size: 24px; /* Adjust size as needed */
-  margin: 0 10px;
-  transition: color 0.3s;
-}
-
-.footer-social a:hover {
-  color: #f28928;
-}
-
-
-    .footer-copy {
-      font-size: 12px;
-      margin-top: -30px;
-    }
-
-    .footer-copy a {
-      color: #f28928;
-      text-decoration: none;
-      transition: color 0.3s;
-    }
-
-    .footer-copy a:hover {
-      color: #ff8c00;
-    }
-  </style>
-
-  <div class="testimonial-container ">
-    <h2 class="testimonial-heading mt-10">Testimoni</h2>
-    <div class="splide">
-      <div class="splide__track">
-        <ul class="splide__list">
-          <li class="splide__slide">
-            <div class="testimonial-card">
-              <div class="testimonial-content">
-                <p>"This is an amazing product! I highly recommend it."</p>
-              </div>
-              <div class="testimonial-author">
-                <div class="author-info">
-                  <p class="author-name">John Doe</p>
-                  <p class="author-role">Customer</p>
-                </div>
-              </div>
-              <span class="arrow-bottom"></span>
+<div class="text-center mt-20 mb-20 px-50 max-w-screen-lg mx-auto">
+  <h2 class="text-4xl font-bold text-gray-800 mb-10">Testimoni</h2>
+  <div class="splide">
+    <div class="splide__track">
+      <ul class="splide__list">
+        <li class="splide__slide flex justify-center items-center p-5">
+          <div class="bg-orange-500 text-white rounded-lg shadow-lg p-6 transform transition-transform duration-300 hover:-translate-y-3 relative mx-3 max-w-md w-full h-[200px] flex flex-col justify-between">
+            <div class="text-lg mb-4 flex-grow">
+              <p>"This is an amazing product! I highly recommend it."</p>
             </div>
-          </li>
-          <li class="splide__slide">
-            <div class="testimonial-card">
-              <div class="testimonial-content">
-                <p>"Excellent service and quality. Will buy again."</p>
+            <div class="flex items-center justify-center mt-1">
+              <div class="text-left">
+                <p class="font-bold text-lg">John Doe</p>
+                <p class="text-gray-200 text-sm">Customer</p>
               </div>
-              <div class="testimonial-author">
-                <div class="author-info">
-                  <p class="author-name">Jane Smith</p>
-                  <p class="author-role">Client</p>
-                </div>
-              </div>
-              <span class="arrow-bottom"></span>
             </div>
-          </li>
-          <li class="splide__slide">
-            <div class="testimonial-card">
-              <div class="testimonial-content">
-                <p>"Another great review here."</p>
-              </div>
-              <div class="testimonial-author">
-                <div class="author-info">
-                  <p class="author-name">Emily Johnson</p>
-                  <p class="author-role">Customer</p>
-                </div>
-              </div>
-              <span class="arrow-bottom"></span>
+            <span class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-5 h-5 bg-orange-500 rotate-45"></span>
+          </div>
+        </li>
+        <li class="splide__slide flex justify-center items-center p-5">
+          <div class="bg-orange-500 text-white rounded-lg shadow-lg p-6 transform transition-transform duration-300 hover:-translate-y-3 relative mx-3 max-w-md w-full h-[200px] flex flex-col justify-between">
+            <div class="text-lg mb-4 flex-grow">
+              <p>"Excellent service and quality. Will buy again."</p>
             </div>
-          </li>
-        </ul>
-      </div>
-      <div class="splide__pagination"></div>
+            <div class="flex items-center justify-center mt-1">
+              <div class="text-left">
+                <p class="font-bold text-lg">Jane Smith</p>
+                <p class="text-gray-200 text-sm">Client</p>
+              </div>
+            </div>
+            <span class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-5 h-5 bg-orange-500 rotate-45"></span>
+          </div>
+        </li>
+        <li class="splide__slide flex justify-center items-center p-5">
+          <div class="bg-orange-500 text-white rounded-lg shadow-lg p-6 transform transition-transform duration-300 hover:-translate-y-3 relative mx-3 max-w-md w-full h-[200px] flex flex-col justify-between">
+            <div class="text-lg mb-4 flex-grow">
+              <p>"Another great review here."</p>
+            </div>
+            <div class="flex items-center justify-center mt-1">
+              <div class="text-left">
+                <p class="font-bold text-lg">Emily Johnson</p>
+                <p class="text-gray-200 text-sm">Customer</p>
+              </div>
+            </div>
+            <span class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-5 h-5 bg-orange-500 rotate-45"></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="splide__pagination flex justify-center mt-8">
+     
     </div>
   </div>
+</div>
 
-  <a 
+<a 
   href="https://wa.me/+628151852575" 
   target="_blank" 
-  class="whatsapp-button first-button"
+  class="fixed bottom-[28vh] right-2 bg-green-500 text-white py-4 px-4 rounded-full shadow-lg border-2 border-green-500 transition-colors duration-300 hover:bg-green-700 z-50"
 >
-  <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-9 h-9 inline-block mr-1">
 </a>
 
+<div class="text-center py-8 px-4 bg-gray-100 text-gray-800">
+  <h2 class="text-2xl font-bold mb-4">Kami Adalah Desainer Terbaik Yang Tak Perlu Diragukan Lagi!</h2>
+  <p class="text-base mb-4">Masih bingung? Langsung konsultasi secara GRATIS oleh tim TUAIDE.</p>
+  <a 
+    href="https://wa.me/+628151852575" 
+    target="_blank" 
+    class="bg-green-500 text-white py-2 px-4 rounded-full shadow-lg flex items-center justify-center mx-auto max-w-xs transition-colors duration-300 hover:bg-green-700"
+  >
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-7 h-7 mr-2">
+    <span class="font-bold text-sm">Hubungi Kami</span>
+  </a>
+</div>
 
-  <div class="kenapa-container">
-    <h2 class="kenapa-heading kanit-bold">Kami Adalah Desainer Terbaik Yang Tak Perlu Diragukan Lagi!</h2>
-    <div class="text">
-      <p>Masih bingung? Langsung konsultasi secara GRATIS oleh tim TUAIDE.</p>
+
+<div class="bg-orange-500 text-black py-10 px-4 playfair-display dm-serif-text-regular">
+  <h2 class="text-5xl font-bold mb-6 text-center">Why Us?</h2>
+  <div class="space-y-1 mx-auto text-sm max-w-4xl">
+    <div class="flex items-center mb-4">
+      <p>Kami menawarkan kepada Anda wawasan baru dalam menyongsong masa depan.</p>
     </div>
-    <!-- Tombol WhatsApp Baru -->
-    <a 
-      href="https://wa.me/+628151852575" 
-      target="_blank" 
-      class="whatsapp-button second-button"
-    >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
-      <span class="whatsapp-text">Hubungi Kami</span>
+    <div class="flex items-center">
+      <i class="fas fa-lightbulb text-yellow-300 mr-1 mb-2"></i>
+      <p>Tidak hanya untuk Memvisualisasikan.</p>
+    </div>
+    <div class="flex items-center">
+      <p class="mb-2">Namun kita juga harus bisa memberikan inovasi.</p>
+    </div>
+    <div class="flex items-center">
+      <i class="fas fa-lightbulb text-yellow-300 mr-1 mb-2"></i>
+      <p class="mb-2">Tidak hanya Berkomunikasi.</p>
+    </div>
+    <div class="flex items-center">
+      <p class="mb-2">Tapi kita juga harus bisa memvisualisasikan apa yang akan kita katakan.</p>
+    </div>
+    <div class="flex items-center">
+      <i class="fas fa-lightbulb text-yellow-300 mr-1 mb-2"></i>
+      <p>Tidak hanya desain.</p>
+    </div>
+    <div class="flex items-center">
+      <p>Namun kita harus memiliki kekuatan komunikasi untuk menyampaikan pesan kepada orang yang tepat.</p>
+    </div>
+  </div>
+</div>
+
+<footer class="bg-gray-800 text-white py-5 px-4 text-center mt-16 relative overflow-hidden">
+  <div class="mb-[0px]">
+    <img src="{terserah}" alt="Company Logo" class="max-w-[100px] justify-left">
+  </div>
+  <div class="mt-6 font-semibold text-xs">
+    <a href="#home" class="text-orange-500 hover:text-yellow-500 mx-1">Home</a>
+    <a href="#about" class="text-orange-500 hover:text-yellow-500 mx-1">About Us</a>
+    <a href="#portofolio" class="text-orange-500 hover:text-yellow-500 mx-1">Portofolio</a>
+    <a href="#contact" class="text-orange-500 hover:text-yellow-500 mx-1">Contact</a>
+  </div>
+  <div class="mt-5 flex justify-center">
+    <a href="https://www.facebook.com/profile.php?id=100077675104283" target="_blank" aria-label="Facebook" class="text-white hover:text-orange-500 mx-2 text-2xl">
+      <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="https://www.instagram.com/tuaide_id?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==/" target="_blank" aria-label="Instagram" class="text-white hover:text-orange-500 mx-2 text-2xl">
+      <i class="fab fa-instagram"></i>
+    </a>
+    <a href="mailto:menanamide@gmail.com" target="_blank" aria-label="Google" class="text-white hover:text-orange-500 mx-2 text-2xl">
+      <i class="fab fa-google"></i>
     </a>
   </div>
-
-
-  <div class="whyus-container">
-    <h2 class="whyus-content playfair-display">Why Us ?</h2>
-    <div class="text-whyus">
-      <p class="Atas playfair-display">
-      Kami menawarkan kepada Anda wawasan baru dalam menyongsong masa depan.
-      </p>
-      <div class="paragrap">
-        <p class="Teks1 playfair-display">
-          <i class="fas fa-lightbulb"></i> Tidak hanya untuk Memvisualisasikan .
-        </p>
-        <p class="Teks2 playfair-display">
-         Namun kita juga harus bisa memberikan inovasi.
-        </p>
-        <p class="Teks1 playfair-display">
-          <i class="fas fa-lightbulb"></i> Tidak hanya Berkomunikasi.
-        </p>
-        <p class="Teks2 playfair-display">
-          Tapi kita juga harus bisa memvisualisasikan apa yang akan kita katakan.
-        </p>
-        <p class="Teks1 playfair-display">
-          <i class="fas fa-lightbulb"></i> Tidak hanya desain.
-        </p>
-        <p class="Teks2 playfair-display">
-        Namun kita harus memiliki kekuatan komunikasi untuk menyampaikan pesan
-        </p>
-        <p class="Teks2 playfair-display">
-      kepada orang yang tepat.
-        </p>
-      </div>
-    </div>
+  <div class="mt-4 text-[12px]">
+    <p>&copy; 2024 Your Company Name. All rights reserved. | <a href="#privacy-policy" class="text-orange-500 hover:text-yellow-500">Privacy Policy</a></p>
   </div>
-  
+</footer>
 
-  <footer class="footer">
-    <div class="footer-logo">
-      <img src="{terserah}" alt="Company Logo">
-    </div>
-    <div class="footer-links">
-      <a href="#home">Home</a>
-      <a href="#about">About Us</a>
-      <a href="#portofolio">Portofolio</a>
-      <a href="#contact">Contact</a>
-    </div>
-    <div class="footer-social">
-      <a href="https://www.facebook.com/profile.php?id=100077675104283" target="_blank" aria-label="Facebook">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="https://www.instagram.com/tuaide_id?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==/" target="_blank" aria-label="Instagram">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="mailto:“menanamide@gmail.com" target="_blank" aria-label="Google">
-        <i class="fab fa-google"></i>
-      </a>
-    </div>
-    <div class="footer-copy">
-      <p>&copy; 2024 Your Company Name. All rights reserved. | <a href="#privacy-policy">Privacy Policy</a></p>
-    </div>
-  </footer>
-  
--- src/lib/components/home/home.svelte
+++ src/lib/components/home/home.svelte
@@ -1,133 +1,181 @@
 <script>
-    import "../../../app.css";
-    import bangor from "$lib/images/lucuanj-mirror.jpg"
-  </script>
-  <style>
-.animated-button {
-  position: relative;
-  display: flex;
-  align-items: center;
-  gap: 2px;
-  padding: 8px 2.7rem;
-  border: 4px solid;
-  border-color: transparent;
-  font-size: 16px;
-  background-color:transparent ;
-  border-radius: 100px;
-  font-weight: 600;
-  color: #aeaeae;
-  box-shadow: 0 0 0 2px #aeaeae;
-  cursor: pointer;
-  overflow: hidden;
-  transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+  import "../../../app.css";
+  import bangor from "$lib/images/lucuanj-mirror.jpg";
+</script>
+
+<style>
+  .animated-button {
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    padding: 8px 2.7rem;
+    border: 4px solid transparent;
+    background-color: transparent;
+    border-radius: 100px;
+    font-weight: 700;
+    color: #cecece;
+    box-shadow: 0 0 0 2px #cecece;
+    cursor: pointer;
+    overflow: hidden;
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button svg {
+    position: absolute;
+    width: 24px;
+    fill: #fffbfb;
+    z-index: 9;
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button .arr-1 {
+    right: 16px;
+  }
+
+  .animated-button .arr-2 {
+    left: -25%;
+  }
+
+  .animated-button .circle {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 26px;
+    height: 26px;
+    background-color: #f28928;
+    border-radius: 50%;
+    opacity: 0;
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button .text {
+    position: relative;
+    z-index: 1;
+    transform: translateX(-12px);
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button:hover {
+    box-shadow: 0 0 0 12px transparent;
+    color: #fff;
+    border-radius: 12px;
+  }
+
+  .animated-button:hover .arr-1 {
+    right: -25%;
+  }
+
+  .animated-button:hover .arr-2 {
+    left: 16px;
+  }
+
+  .animated-button:hover .text {
+    transform: translateX(12px);
+  }
+
+  .animated-button:hover svg {
+    fill: #fff;
+  }
+
+  .animated-button:active {
+    scale: 0.95;
+    box-shadow: 0 0 0 4px #fff;
+  }
+
+  .animated-button:hover .circle {
+    width: 220px;
+    height: 220px;
+    opacity: 1;
+  }
+
+  /* Media Queries for Responsive Design */
+
+  /* Media Queries for Responsive Design */
+
+@media (max-width: 640px) {
+  .animated-button {
+    padding: 6px 1.5rem;
+    font-size: 0.875rem; /* text-sm */
+  }
+
+  .animated-button svg {
+    width: 20px;
+  }
+
+  .animated-button .circle {
+    width: 20px;
+    height: 20px;
+  }
 }
-.animated-button svg {
-  position: absolute;
-  width: 24px;
-  fill: #aeaeae;
-  z-index: 9;
-  transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+
+@media (min-width: 641px) and (max-width: 1024px) {
+  .animated-button {
+    padding: 7px 2rem;
+    font-size: 1rem; /* text-base */
+  }
+
+  .animated-button svg {
+    width: 22px;
+  }
+
+  .animated-button .circle {
+    width: 24px;
+    height: 24px;
+  }
+
+  /* Adjusting hero section for tablets */
 }
-.animated-button .arr-1 {
-  right: 16px;
+
+@media (min-width: 1025px) {
+  .animated-button {
+    padding: 8px 2.7rem;
+  }
 }
-.animated-button .arr-2 {
-  left: -25%;
-}
-.animated-button .circle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 26px;
-  height: 26px;
-  background-color: #f28928;
-  border-radius: 50%;
-  opacity: 0;
-  transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
-}
-.animated-button .text {
-  position: relative;
-  z-index: 1;
-  transform: translateX(-12px);
-  transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
-}
-.animated-button:hover {
-  box-shadow: 0 0 0 12px transparent;
-  color: #fff;
-  border-radius: 12px;
-}
-.animated-button:hover .arr-1 {
-  right: -25%;
-}
-.animated-button:hover .arr-2 {
-  left: 16px;
-}
-.animated-button:hover .text {
-  transform: translateX(12px);
-}
-.animated-button:hover svg {
-  fill: #fff;
-}
-.animated-button:active {
-  scale: 0.95;
-  box-shadow: 0 0 0 4px #fff;
-}
-.animated-button:hover .circle {
-  width: 220px;
-  height: 220px;
-  opacity: 1;
-}
+
 </style>
-  <!------Background-->
-  <div class="h-screen w-full mx-auto relative bg-image bg-cover bg-no-repeat bg-center bg-opacity-50" style="background-image: url({bangor});">
-    <div class="absolute inset-0 bg-black opacity-55 mx-auto"></div>
 
-  <!--Content Hero-->
-  <div class="w-3/4 relative mx-auto h-auto justify-center">
-    <div class="mx-auto items-cemter relative py-[10rem]">
-      <div class="w-3/4 mx-auto h-[800px] flex">
-        <!--Text Content-->
-        <div class="flex flex-col w-1/2 mx-auto">
-          <h1 class="text-white text-[3rem] mb-5 ubuntu-bold pr-[1rem] mt-[6.5rem]">
-            Transformasi Merek Anda dengan Sentuhan Desain Grafis yang Memukau!
-          </h1>
-          <p class="text-[#aeaeae] text-[1rem] ubuntu-bold max-w-[400px]">
-            Butuh visual yang memukau? <br>
-            Tuaide siap membantu. Dengan tim desainer profesional, kami akan menerjemahkan 
-            visi Anda menjadi desain grafis yang  <br />
-            kuat dan efektif.
-            Biarkan kami menjadi partner kreatifmu.
-          </p>
+<div class="h-1/2 w-full mx-auto relative bg-cover bg-no-repeat bg-center bg-opacity-50" style="background-image: url({bangor});">
+  <div class="absolute inset-0 opacity-55"></div>
 
-            <!--Button Banner-->
-            <div class="w-[11rem] mt-[2.5rem]"> 
-                <button class="animated-button">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="arr-2" viewBox="0 0 24 24">
-                    <path
-                      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                    ></path>
-                  </svg>
-                  <span class="text">Contact Us</span>
-                  <span class="circle"></span>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="arr-1" viewBox="0 0 24 24">
-                    <path
-                      d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                    ></path>
-                  </svg>
-              </button>
-            </div>
+  <!-- Content Hero -->
+  <div class="w-full max-w-6xl mx-auto h-auto flex flex-col items-center justify-center py-20 px-6 lg:py-32 lg:px-12">
+    <div class="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center">
+      <!-- Text Content -->
+      <div class="flex flex-col lg:w-1/2 mb-[20px] lg:mb-0 lg:text-left">
+        <h1 class="text-[#cecece] text-2xl lg:text-5xl mb-10 font-bold leading-tight">
+          Transformasi Merek Anda dengan Sentuhan Desain Grafis yang Memukau!
+        </h1>
+        <p class="text-[#cecece] text-sm lg:text-xl max-w-md mx-auto lg:mx-0">
+          Butuh visual yang memukau? <br>
+          Tuaide siap membantu. Dengan tim desainer profesional, kami akan menerjemahkan 
+          visi Anda menjadi desain grafis yang <br>
+          kuat dan efektif.
+          Biarkan kami menjadi partner kreatifmu.
+        </p>
+
+        <!-- Button Banner -->
+        <div class="mt-10">
+          <button class="animated-button px-4 py-2 text-base lg:text-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" class="arr-2" viewBox="0 0 24 24">
+              <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+            </svg>
+            <span class="text">Contact Us</span>
+            <span class="circle"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="arr-1" viewBox="0 0 24 24">
+              <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+            </svg>
+          </button>
         </div>
-        <!--dummby-->
-        <div class="flex w-1/2 mx-auto">
-        </div>
-      <!--Close Div image banner-->
+      </div>
+
+      <!-- Dummy -->
+      <div class="hidden lg:flex lg:w-1/2">
+        <!-- Empty space for potential image or content -->
       </div>
     </div>
   </div>
-  </div>
-  
+</div>
 
 <slot></slot>
-
-  
