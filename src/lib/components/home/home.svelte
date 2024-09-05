a754e1064349906bf219b9ff9a2bc7b1ae20881e
erza <satriaseptian724@gmail.com>
2024-09-05 15:56:54 +0700

    terserah

--- src/lib/components/home/home.svelte
+++ src/lib/components/home/home.svelte
@@ -1,7 +1,14 @@
-<script>
+<script lang="ts">
   import "../../../app.css";
   import bangor from "$lib/images/lucuanj-mirror.jpg";
+
+  // Function to handle button click
+  function handleButtonClick() {
+    console.log('Button clicked'); // Add this to check if the function is being called
+    window.location.href = 'https://wa.me/+628151852575';
+  }
 </script>
+
 
 <style>
   .animated-button {
@@ -160,7 +167,7 @@
 
         <!-- Button Banner -->
         <div class="mt-[2rem]">
-          <button class="animated-button px-4 py-2 text-base lg:text-lg">
+          <button class="animated-button px-4 py-2 text-base lg:text-lg" on:click={handleButtonClick} style="outline: none;">
             <svg xmlns="http://www.w3.org/2000/svg" class="arr-2" viewBox="0 0 24 24">
               <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
             </svg>
